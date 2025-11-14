d48b17f49da131790f8e5974cbb383e3c077ea94
atharvabalpandebatch2025 <atharva.balpande.batch2025@sitnagpur.siu.edu.in>
2025-11-14 11:14:50 +0530

    practical8.c

--- practical8.c
+++ practical8.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 
+// Structure to store distance
 struct Distance {
     int feet;
     float inch;
 };
 
 int main() {
+
     struct Distance d1, d2, sum;
+
+    // ---- DISTANCE INPUT ----
     printf("Enter 1st distance (feet inch): ");
     scanf("%d %f", &d1.feet, &d1.inch);
+
     printf("Enter 2nd distance (feet inch): ");
     scanf("%d %f", &d2.feet, &d2.inch);
 
+    // ---- ADDING DISTANCES ----
     sum.feet = d1.feet + d2.feet;
     sum.inch = d1.inch + d2.inch;
+
     if (sum.inch >= 12) {
         sum.inch -= 12;
         sum.feet++;
     }
-    printf("Sum = %d'-%.1f\"\n", sum.feet, sum.inch);
 
+    printf("\nSum of Distances = %d' - %.1f\"\n", sum.feet, sum.inch);
+
+    // ---- CGPA CALCULATION ----
     int n, i;
-    float marks, total = 0;
+    float cgpa, total = 0;
+
     printf("\nEnter number of subjects: ");
     scanf("%d", &n);
+
     for (i = 0; i < n; i++) {
         printf("Enter CGPA of subject %d: ", i + 1);
-        scanf("%f", &marks);
-        total += marks;
+        scanf("%f", &cgpa);
+        total += cgpa;
     }
+
     printf("Overall CGPA = %.2f\n", total / n);
+
     return 0;
 }
-
