b8a08ef0a677ce2ab551d5214c04761d50a484d7
atharvabalpandebatch2025 <atharva.balpande.batch2025@sitnagpur.siu.edu.in>
2025-11-14 11:12:52 +0530

    practical6.c

--- practical6.c
+++ practical6.c
@@ -2,45 +2,76 @@
 #include <math.h>
 
 int main() {
+
     int choice;
-    printf("1. Simple Interest\n2. Prime Number\n3. Armstrong Number\nEnter choice: ");
+
+    // ---- MENU ----
+    printf("1. Simple Interest\n");
+    printf("2. Prime Number\n");
+    printf("3. Armstrong Number\n");
+    printf("Enter choice: ");
     scanf("%d", &choice);
 
     switch (choice) {
+
+        // ---- SIMPLE INTEREST ----
         case 1: {
             float p, r, t, si;
-            printf("Enter P, R, T: ");
+            printf("Enter P R T: ");
             scanf("%f %f %f", &p, &r, &t);
+
             si = (p * r * t) / 100;
             printf("Simple Interest = %.2f\n", si);
             break;
         }
+
+        // ---- PRIME NUMBER ----
         case 2: {
             int n, i, flag = 0;
             printf("Enter number: ");
             scanf("%d", &n);
-            for (i = 2; i <= n / 2; i++)
-                if (n % i == 0) { flag = 1; break; }
-            if (flag == 0 && n > 1) printf("Prime\n");
-            else printf("Not Prime\n");
+
+            for (i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 0 && n > 1)
+                printf("Prime\n");
+            else
+                printf("Not Prime\n");
+
             break;
         }
+
+        // ---- ARMSTRONG NUMBER ----
         case 3: {
-            int n, sum = 0, temp, r;
+            int n, temp, r, sum = 0;
             printf("Enter number: ");
             scanf("%d", &n);
+
             temp = n;
+
             while (n > 0) {
                 r = n % 10;
-                sum += r*r*r;
+                sum += r * r * r;
                 n /= 10;
             }
-            if (sum == temp) printf("Armstrong\n");
-            else printf("Not Armstrong\n");
+
+            if (sum == temp)
+                printf("Armstrong\n");
+            else
+                printf("Not Armstrong\n");
+
             break;
         }
+
+        // ---- INVALID ----
         default:
             printf("Invalid choice\n");
     }
+
     return 0;
 }
